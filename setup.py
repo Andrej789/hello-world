36ff8e2f759ebf87c354a72f62b9b30614657f9b
Andrej_dell_e5570 <akubek@gmail.com>
2019-11-01 16:24:28 +0100

    Seyup file created

--- setup.py
+++ setup.py
@@ -1,3 +1,12 @@
 from setuptools import setup
 
-setup()
+setup(
+    name='hello-world',
+    version='0.1',
+    description='Hello World Example',
+    author='Andrej',
+    author_email='akubek@gmail.com',
+    license='Public Domain',
+    url='https://gist.github.com/63fb02bf7fca9c9d68ee3b4ad942d43e.git',
+    py_modules=['hello-world.py'],
+)
